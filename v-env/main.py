a871499edff98537209cc767ff9c67ee211c6cd4
Jason YI Jiang <jiangkaiyi@jiangkaiyide-MacBook-Pro.local>
2019-03-25 14:18:45 +0800

    Second Draft

--- v-env/main.py
+++ v-env/main.py
@@ -5,8 +5,8 @@ import time
 import requests
 
 def control(argv):
-  if len(argv[1].split('.')) == 4:
-    result = os.popen("adb connect " + argv[1]).read()
+  if len(argv.split('.')) == 4:
+    result = os.popen("adb connect " + argv).read()
     fileList = []
     # print(result)
     if 'unable' in result:
@@ -18,17 +18,23 @@ def control(argv):
         if 'txt' in file:
           fileList.append(file.split('\n')[0])
       # print(fileList)
-      os.popen("adb pull /sdcard/Android/data/swpc.wistron.com.wiwall.tv/files/logs/" + fileList[0] + " ./recordfile")
       time.sleep(2)
-      os.rename('./recordfile/'+ fileList[0], './recordfile/' + argv[1] + '_' + fileList[0])
-      os.popen("adb shell \"su 0 rm -rf /sdcard/Android/data/swpc.wistron.com.wiwall.tv/files/logs/" + fileList[0] + "\"")
-      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/cache\"")
-      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/code_cache\"")
-      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/databases\"")
-      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/files\"")
-      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/shared_prefs\"")
-      os.popen("adb reboot")
-      return fileList[0]
+      os.popen("adb pull /sdcard/Android/data/swpc.wistron.com.wiwall.tv/files/logs/" + fileList[0] + " ./recordfile")
+      time.sleep(4)
+      os.rename('./recordfile/'+ fileList[0], './recordfile/' + argv + '_' + fileList[0])
+      #os.popen("adb shell \"su 0 rm -rf /sdcard/Android/data/swpc.wistron.com.wiwall.tv/files/logs/" + fileList[0] + "\"")
+      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/cache\"")
+      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/code_cache\"")
+      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/databases\"")
+      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/files\"")
+      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/shared_prefs\"")
+      time.sleep(2)
+      os.popen("adb shell \"su 0 rm -rf /sdcard/WiWall/Download\"")
+      time.sleep(2)
+      #os.popen("adb reboot")
+      os.popen("adb disconnect")
+      time.sleep(1)
+      return argv + '_' + fileList[0]
   else:
     print('Your ip address is invalid, please try again.')
     return "null"
@@ -39,10 +45,10 @@ def processTime(file):
   tempTime = ''
   f = open('./recordfile/' + file)
   for line in f:
-    if "pull" in line:
+    if "enque" in line:
       tempTime = (line.split(' ')[1]).split('.')[0]
       start_time = int(tempTime.split(':')[0]) * 3600 + int(tempTime.split(':')[1]) * 60 + int(tempTime.split(':')[2])
-    if "completeAll" in line:
+    if "downloadComplete" in line:
       tempTime = (line.split(' ')[1]).split('.')[0]
       end_time = int(tempTime.split(':')[0]) * 3600 + int(tempTime.split(':')[1]) * 60 + int(tempTime.split(':')[2])
   return secondToDateString(end_time - start_time)
@@ -78,17 +84,19 @@ if __name__ == "__main__":
   ipArr = config["model"]["ip"]
   modelName = config["name"]
   #### process lambda log file ####
-  json_result = fetch_api_request(modelName)
-  lambda_start_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_start_time"]).split('T')[1]).split('.')[0])
-  lambda_end_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_update_time"]).split('T')[1]).split('.')[0])
-  time1 = secondToDateString(lambda_end_time - lambda_start_time)
-  print("AWS Lambda Splitting Cost Time : " + time1)
+  #json_result = fetch_api_request(modelName)
+  #lambda_start_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_start_time"]).split('T')[1]).split('.')[0])
+  #lambda_end_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_update_time"]).split('T')[1]).split('.')[0])
+  #time1 = secondToDateString(lambda_end_time - lambda_start_time)
+  #print("AWS Lambda Splitting Cost Time : " + time1)
   #################################
   for ip in ipArr:
     print("============   " + ip + "   ===============")
     resultText = control(ip)
     time.sleep(2)
     time2 = processTime(resultText)
+    #totalTime = secondToDateString(timeStrToInt(time1) + timeStrToInt(time2))
+    totalTime = time2
     print("Android download cost : " + time2)
-    print("Total Cost : " + secondToDateString(timeStrToInt(time1) + timeStrToInt(time2)))
+    print("Total Cost : " + totalTime)
     print("============================================")
