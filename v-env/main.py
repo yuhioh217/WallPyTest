e760e7f1dc0168da6f3a8b9b314a4d0e7146ecfc
Jason YI Jiang <jiangkaiyi@jiangkaiyide-MacBook-Pro.local>
2019-03-25 17:18:08 +0800

    fix txt format and date time

--- v-env/main.py
+++ v-env/main.py
@@ -8,7 +8,6 @@ def control(argv):
   if len(argv.split('.')) == 4:
     result = os.popen("adb connect " + argv).read()
     fileList = []
-    # print(result)
     if 'unable' in result:
       print('connection failed.')
     elif 'connected' in result:
@@ -17,7 +16,6 @@ def control(argv):
       for file in (list.split(' ')):
         if 'txt' in file:
           fileList.append(file.split('\n')[0])
-      # print(fileList)
       time.sleep(2)
       os.popen("adb pull /sdcard/Android/data/swpc.wistron.com.wiwall.tv/files/logs/" + fileList[0] + " ./recordfile")
       time.sleep(4)
@@ -43,24 +41,26 @@ def processTime(file):
   start_time = 0
   end_time = 0
   tempTime = ''
-  f = open('./recordfile/' + file)
+  f = open('./recordfile/' + file, encoding='utf-8',errors='ignore')
   for line in f:
     if "enque download" in line:
-      tempTime = (line.split(' ')[1]).split('.')[0]
-      start_time = int(tempTime.split(':')[0]) * 3600 + int(tempTime.split(':')[1]) * 60 + int(tempTime.split(':')[2])
+      tempTime = (line.split(' ')[1])
+      start_time = int(tempTime.split(':')[0]) * 3600000 + int(tempTime.split(':')[1]) * 60000 + int(float(tempTime.split(':')[2])*1000)
+      #print(int(float(tempTime.split(':')[2])*1000))
     if "downloadComplete" in line:
-      tempTime = (line.split(' ')[1]).split('.')[0]
-      end_time = int(tempTime.split(':')[0]) * 3600 + int(tempTime.split(':')[1]) * 60 + int(tempTime.split(':')[2])
+      tempTime = (line.split(' ')[1])
+      end_time = int(tempTime.split(':')[0]) * 3600000 + int(tempTime.split(':')[1]) * 60000 + int(float(tempTime.split(':')[2])*1000)
+      #print(int(float(tempTime.split(':')[2])*1000))
+  print(secondToDateString(end_time - start_time))
   return secondToDateString(end_time - start_time)
 
 def timeStrToInt(timeStr):
-  return int(timeStr.split(':')[0]) * 3600 + int(timeStr.split(':')[1]) * 60 + int(timeStr.split(':')[2])
+  return int(timeStr.split(':')[0]) * 3600000 + int(timeStr.split(':')[1]) * 60000 + int(timeStr.split(':')[2])
 
 def secondToDateString(value):
-  hourInt = int(value/3600)
-  minInt  = int((value - (hourInt*3600))/60)
-  secInt  = int(value - hourInt*3600 - minInt*60)
-  # print(str(hourInt) , ':' , str(minInt) , ':' , str(secInt))
+  hourInt = int(value/3600000)
+  minInt  = int((value - (hourInt*3600000))/60000)
+  secInt  = int(value - hourInt*3600000 - minInt*60000)
   return str(hourInt) + ':' + str(minInt) + ':' + str(secInt)
 
 def loadSetting():
@@ -80,8 +80,12 @@ def fetch_api_request(modelName):
 def aws_lambda_timing(modelName):
   try:
     json_result = fetch_api_request(modelName)
-    lambda_start_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_start_time"]).split('T')[1]).split('.')[0])
-    lambda_end_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_update_time"]).split('T')[1]).split('.')[0])
+    lambda_start_time_str = (str((((json_result[0])["playlistData"])[0])["split_info"]["s_start_time"]).split('T')[1]).split('Z')[0]
+    lambda_end_time_str = (str((((json_result[0])["playlistData"])[0])["split_info"]["s_update_time"]).split('T')[1]).split('Z')[0]
+    #print(lambda_start_time_str)
+    #print(lambda_end_time_str)
+    lambda_start_time = int(lambda_start_time_str.split(':')[0]) * 3600000 + int(lambda_start_time_str.split(':')[1]) * 60000 + int(float(lambda_start_time_str.split(':')[2])*1000)
+    lambda_end_time = int(lambda_end_time_str.split(':')[0]) * 3600000 + int(lambda_end_time_str.split(':')[1]) * 60000 + int(float(lambda_end_time_str.split(':')[2])*1000)
     time1 = secondToDateString(lambda_end_time - lambda_start_time)
     print("AWS Lambda Splitting Cost Time : " + time1)
     return time1
@@ -90,21 +94,20 @@ def aws_lambda_timing(modelName):
 
 
 if __name__ == "__main__":
-  #control(sys.argv)
-  #processTime("192.168.1.3_0322_200754.txt")
+
   config = loadSetting()
   ipArr = config["model"]["ip"]
   modelName = config["name"]
   #### process lambda log file ####
-  aws_lambda_timing(modelName)
+  time1 = aws_lambda_timing(modelName)
   #################################
   for ip in ipArr:
     print("============   " + ip + "   ==============")
     resultText = control(ip)
     time.sleep(2)
     time2 = processTime(resultText)
-    #totalTime = secondToDateString(timeStrToInt(time1) + timeStrToInt(time2))
-    totalTime = time2
+    totalTime = secondToDateString(timeStrToInt(time1) + timeStrToInt(time2))
+    #totalTime = time2
     print("Android download cost : " + time2)
     print("Total Cost : " + totalTime)
     print("============================================")
