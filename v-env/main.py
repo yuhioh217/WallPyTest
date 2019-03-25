de6f61febf3b6e60467cb1ce7bc8fe3156b4df76
Jason YI Jiang <jiangkaiyi@jiangkaiyide-MacBook-Pro.local>
2019-03-25 14:58:28 +0800

    splite the aws_lambda function

--- v-env/main.py
+++ v-env/main.py
@@ -22,16 +22,16 @@ def control(argv):
       os.popen("adb pull /sdcard/Android/data/swpc.wistron.com.wiwall.tv/files/logs/" + fileList[0] + " ./recordfile")
       time.sleep(4)
       os.rename('./recordfile/'+ fileList[0], './recordfile/' + argv + '_' + fileList[0])
-      #os.popen("adb shell \"su 0 rm -rf /sdcard/Android/data/swpc.wistron.com.wiwall.tv/files/logs/" + fileList[0] + "\"")
-      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/cache\"")
-      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/code_cache\"")
-      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/databases\"")
-      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/files\"")
-      #os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/shared_prefs\"")
+      os.popen("adb shell \"su 0 rm -rf /sdcard/Android/data/swpc.wistron.com.wiwall.tv/files/logs/" + fileList[0] + "\"")
+      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/cache\"")
+      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/code_cache\"")
+      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/databases\"")
+      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/files\"")
+      os.popen("adb shell \"su 0 rm -rf /data/data/swpc.wistron.com.wiwall.tv/shared_prefs\"")
       time.sleep(2)
       os.popen("adb shell \"su 0 rm -rf /sdcard/WiWall/Download\"")
       time.sleep(2)
-      #os.popen("adb reboot")
+      os.popen("adb reboot")
       os.popen("adb disconnect")
       time.sleep(1)
       return argv + '_' + fileList[0]
@@ -45,7 +45,7 @@ def processTime(file):
   tempTime = ''
   f = open('./recordfile/' + file)
   for line in f:
-    if "enque" in line:
+    if "enque download" in line:
       tempTime = (line.split(' ')[1]).split('.')[0]
       start_time = int(tempTime.split(':')[0]) * 3600 + int(tempTime.split(':')[1]) * 60 + int(tempTime.split(':')[2])
     if "downloadComplete" in line:
@@ -77,6 +77,18 @@ def fetch_api_request(modelName):
   # print(r.json())
   return r.json()
 
+def aws_lambda_timing(modelName):
+  try:
+    json_result = fetch_api_request(modelName)
+    lambda_start_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_start_time"]).split('T')[1]).split('.')[0])
+    lambda_end_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_update_time"]).split('T')[1]).split('.')[0])
+    time1 = secondToDateString(lambda_end_time - lambda_start_time)
+    print("AWS Lambda Splitting Cost Time : " + time1)
+    return time1
+  except:
+    return "0:0:0"
+
+
 if __name__ == "__main__":
   #control(sys.argv)
   #processTime("192.168.1.3_0322_200754.txt")
@@ -84,14 +96,10 @@ if __name__ == "__main__":
   ipArr = config["model"]["ip"]
   modelName = config["name"]
   #### process lambda log file ####
-  #json_result = fetch_api_request(modelName)
-  #lambda_start_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_start_time"]).split('T')[1]).split('.')[0])
-  #lambda_end_time = timeStrToInt((str((((json_result[0])["playlistData"])[0])["split_info"]["s_update_time"]).split('T')[1]).split('.')[0])
-  #time1 = secondToDateString(lambda_end_time - lambda_start_time)
-  #print("AWS Lambda Splitting Cost Time : " + time1)
+  aws_lambda_timing(modelName)
   #################################
   for ip in ipArr:
-    print("============   " + ip + "   ===============")
+    print("============   " + ip + "   ==============")
     resultText = control(ip)
     time.sleep(2)
     time2 = processTime(resultText)
